e4580d60f8417a7f3cbdaffd46d75616bc8ce612
Charles Lehner <cel@celehner.com>
2015-01-24 04:50:46 -0500

    Minor refactor user command handling

--- server.sh
+++ server.sh
@@ -20,26 +20,30 @@ write_client() {
 	echo $@ >> "${client_socks[$client_id]}"
 }
 
+# Handle command sent by client
+handle_user_command() {
+	local client_id="$1"; shift
+	local cmd="$1"; shift
+	echo $client_id $cmd $@
+	case "$cmd" in
+		new)
+			client_socks[$client_id]=$1
+			write_client $client_id welcome
+			;;
+		move)
+			write_client $client_id moved $@
+			;;
+		*)
+			write_client $client_id unknown $cmd
+			;;
+	esac
+}
+
 # Read from server socket
 while exec <"$1"
 do
 	# Read commands from clients
-	while read -r client_id cmd arg1 arg2 args
-	do
-		case "$cmd" in
-			new)
-				echo new player $client_id: $arg1
-				client_socks[$client_id]=$arg1
-				write_client $client_id welcome
-				;;
-			move)
-				echo move player $client_id: $arg1 $arg2
-				write_client $client_id moved $args
-				;;
-			*)
-				echo player $client_id: $cmd
-				write_client $client_id unknown $cmd $arg1 $args
-				;;
-		esac
+	while read -r args
+	do handle_user_command $args
 	done
 done

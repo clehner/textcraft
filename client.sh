f7d3825f0244c8329ceea8dbfb435ecbab60f2c5
Charles Lehner <cel@celehner.com>
2015-01-24 04:11:12 -0500

    Move more control in remote (local client)

--- client.sh
+++ client.sh
@@ -13,55 +13,25 @@ test -p "$1" || {
 
 player_id=p$$
 
-echo "Connecting to server"
+echo conn connecting
 exec 3>"$1"
-echo connected
-
-server_write() {
-	echo $player_id $@ >&3
-}
+echo conn connected
 
 # Read commands from server
 {
 	client_sock=$(mktemp -u)
 	mkfifo "$client_sock"
 	trap "rm $client_sock" INT
-	while read -r cmd data
-	do
-		echo got server cmd "$cmd" "$data"
-	done <"$client_sock"
-	echo "Connection to server lost"
+	# Notify server that we are here
+	echo "$player_id" "$client_sock" >&3
+	# Read responses from server
+	cat "$client_sock"
+	echo conn disconnected
 	exit
 } &
 trap "kill -TERM $!" EXIT INT
 
-YELLOW="\e[1;33m"
-RESET="\e[0m"
-echo -e hi $YELLOW asd $RESET
-
-player_move() {
-	server_write move "$1" "$2"
-	echo move! $1 $2
-	case "$1 $2" in
-		"-1 0")
-			#echo -e "\e[<3>Aok"
-			;;
-	esac
-}
-
-stty -echo
-fix_color() {
-	stty echo
-	exit
-}
-trap fix_color INT TERM
-
-while read -rn 1 char
-do
-	case "$char" in
-		j) player_move 1 0;;
-		k) player_move -1 0;;
-		h) player_move 0 -1;;
-		l) player_move 0 1;;
-	esac
-done
+# Transfer user commands to server
+while read cmd
+do echo "$player_id" "$cmd"
+done >&3
--- remote.sh
+++ remote.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
 # remote.sh - connect to the remote server
+
+test $# -ge 2 || {
+	echo "Usage: ./remote.sh <host> <port>"
+	exit 1
+}
+
 stty -echo
-while read -rn1 char
-do echo "$char"
-done | nc $*
+YELLOW="\e[1;33m"
+RESET="\e[0m"
+
+# Connect to server
+exec 3<> /dev/tcp/$1/$2
+
+# Read commands from server
+while read -r cmd data
+do
+	echo from server: $cmd $data
+done &
+
+server_write() {
+	echo $player_id $@ >&3
+}
+
+player_move() {
+	server_write move "$1" "$2"
+	echo move! $1 $2
+	case "$1 $2" in
+		"-1 0")
+			#echo -e "\e[<3>Aok"
+			;;
+	esac
+}
+
+stty -echo
+fix_color() {
+	stty echo
+	exit
+}
+trap fix_color INT TERM
+
+# Read from user's keyboard
+while read -rn 1 char
+do
+	case "$char" in
+		j) player_move 1 0;;
+		k) player_move -1 0;;
+		h) player_move 0 -1;;
+		l) player_move 0 1;;
+	esac
+done

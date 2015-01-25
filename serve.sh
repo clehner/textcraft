4e47563ba483dc5a925e01494972fa8533b5753a
Charles Lehner <cel@celehner.com>
2015-01-25 03:26:08 -0500

    Exit when nc server finishes

--- serve.sh
+++ serve.sh
@@ -4,18 +4,19 @@
 server_sock=$(mktemp -u)
 mkfifo "$server_sock"
 
-echo Starting network server on port ${PORT:=9000}
-nc --continuous -lp $PORT -e "./connection.sh $server_sock" &
-NC_PID=$!
-trap cleanup INT
-
 cleanup() {
-	echo killing $NC_PID
-	kill -9 $NC_PID
-	echo removing $server_sock
-	rm "$server_sock"
+	kill -9 $NC_PID 2>&-
+	rm -f "$server_sock"
 	exit
 }
+
+echo Starting network server on port ${PORT:=9000}
+{
+	nc --continuous -lp $PORT -e "./connection.sh $server_sock"
+	cleanup
+} &
+NC_PID=$!
+trap cleanup INT
 
 echo Starting game server at $server_sock.
 ./server.sh "$server_sock"
